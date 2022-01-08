0722474d0986b8c5997748ee1c2241d113ca4295
raaraya1 <raaraya1@miuandes.cl>
2022-01-08 20:28:53 -0300

    fix error conexion postgresql

--- app.py
+++ app.py
@@ -78,11 +78,11 @@ with st.expander('Conectar con una base de datos'):
         c1.image(img_logo)
         choice = st.radio('Conectar base de datos:', ['Interna', 'Externa'])
         if choice == 'Interna':
-            Postgre = SQL(data_base=st.secrets["postgres"].dbname,
-                    user_name=st.secrets["postgres"].user,
-                    password=st.secrets["postgres"].password,
-                    host=st.secrets["postgres"].host,
-                    port=st.secrets["postgres"].port)
+            Postgre = SQL(data_base=st.secrets["postgres"]["dbname"],
+                    user_name=st.secrets["postgres"]["user"],
+                    password=st.secrets["postgres"]["password"],
+                    host=st.secrets["postgres"]["host"],
+                    port=st.secrets["postgres"]["port"])
         elif choice == 'Externa':
             host = c2.text_input('Nombre del host')
             port = c2.text_input('Puerto')
