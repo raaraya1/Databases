2e2e257886a85fdf1979387c6b2773b2a249762c
raaraya1 <raaraya1@miuandes.cl>
2022-01-09 19:54:10 -0300

    test sqlite conection

--- app.py
+++ app.py
@@ -117,8 +117,8 @@ with st.expander('Conectar con una base de datos'):
         c1.image(img_logo)
         choice = st.radio('Conectar base de datos:', ['Externa', 'Interna'])
         if choice == 'Interna': database_path = 'database.db'
-        elif choice == 'Externa': database_path = c2.text_input('Ruta de la base de datos')
-        
+        elif choice == 'Externa': database_path = c2.file_uploader('Subir archivo de la base de datos')
+
 if work_choice == 'PostgreSQL':
     if database_name != '' and user_name != '' and password != '' and host != '' and port != '':
         Postgre = SQL(database_name, user_name, password, host, port)
